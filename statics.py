4c94e9f013f12c4124aab9b82f5083b5f2162f8f
Uma Balannavar <150453822+UmaBalannavar@users.noreply.github.com>
2026-02-23 03:38:41 +0530

    Add files via upload

---
+++ statics.py
@@ -0,0 +1,94 @@
+import numpy
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+# Shows plots in jupyter notebook
+
+
+# Set plot style
+sns.set(color_codes=True)
+client_df = pd.read_csv('./client_data.csv')
+price_df = pd.read_csv('./price_data.csv')
+
+client_df.head(3)
+price_df.head(3)
+
+client_df.info()
+price_df.info()
+
+client_df.describe()
+price_df.describe()
+
+def plot_stacked_bars(dataframe, title_, size_=(18, 10), rot_=0, legend_="upper right"):
+    """
+    Plot stacked bars with annotations
+    """
+    ax = dataframe.plot(
+        kind="bar",
+        stacked=True,
+        figsize=size_,
+        rot=rot_,
+        title=title_
+    )
+
+    # Annotate bars
+    annotate_stacked_bars(ax, textsize=14)
+    # Rename legend
+    plt.legend(["Retention", "Churn"], loc=legend_)
+    # Labels
+    plt.ylabel("Company base (%)")
+    plt.show()
+
+def annotate_stacked_bars(ax, pad=0.99, colour="white", textsize=13):
+    """
+    Add value annotations to the bars
+    """
+
+    # Iterate over the plotted rectanges/bars
+    for p in ax.patches:
+        
+        # Calculate annotation
+        value = str(round(p.get_height(),1))
+        # If value is 0 do not annotate
+        if value == '0.0':
+            continue
+        ax.annotate(
+            value,
+            ((p.get_x()+ p.get_width()/2)*pad-0.05, (p.get_y()+p.get_height()/2)*pad),
+            color=colour,
+            size=textsize
+        )
+
+def plot_distribution(dataframe, column, ax, bins_=50):
+    """
+    Plot variable distirbution in a stacked histogram of churned or retained company
+    """
+    # Create a temporal dataframe with the data to be plot
+    temp = pd.DataFrame({"Retention": dataframe[dataframe["churn"]==0][column],
+    "Churn":dataframe[dataframe["churn"]==1][column]})
+    # Plot the histogram
+    temp[["Retention","Churn"]].plot(kind='hist', bins=bins_, ax=ax, stacked=True)
+    # X-axis label
+    ax.set_xlabel(column)
+    # Change the x-axis to plain style
+    ax.ticklabel_format(style='plain', axis='x')
+
+
+
+churn = client_df[['id', 'churn']]
+churn.columns = ['Companies', 'churn']
+churn_total = churn.groupby(churn['churn']).count()
+churn_percentage = churn_total / churn_total.sum() * 100
+plot_stacked_bars(churn_percentage.transpose(), "Churning status", (5, 5), legend_="lower right")
+
+
+consumption = client_df[['id', 'cons_12m', 'cons_gas_12m', 'cons_last_month', 'imp_cons', 'has_gas', 'churn']]
+
+fig, axs = plt.subplots(nrows=1, figsize=(18, 5))
+
+plot_distribution(consumption, 'cons_12m', axs)
+
+client_df = pd.read_csv('./client_data.csv')
+price_df = pd.read_csv('./price_data.csv')
+
